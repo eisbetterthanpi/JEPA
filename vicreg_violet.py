0213ec1533bff8b1474122b95b5b1b6f8e8af62c
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-03-26 15:58:50 +0800

    Create vicreg_violet.py

---
+++ vicreg_violet.py
@@ -0,0 +1,157 @@
+# @title TransformerVICReg
+import torch
+from torch import nn
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class TransformerVICReg(nn.Module):
+    def __init__(self, in_dim, d_model, out_dim=None, d_head=4, d_hid=None, nlayers=1, drop=0):
+        super().__init__()
+        act = nn.GELU()
+        patch_size=4
+        self.embed = nn.Sequential(
+            # nn.Linear(in_dim, d_model), act
+            # nn.Conv1d(in_dim, d_model,3,2,3//2), nn.BatchNorm1d(d_model), nn.ReLU(), nn.MaxPool1d(2,2),
+            # nn.Conv1d(d_model, d_model,3,2,3//2), nn.BatchNorm1d(d_model), nn.ReLU(), nn.MaxPool1d(2,2),
+            # nn.Conv1d(d_model, d_model,3,2,3//2),
+            nn.Conv2d(in_dim, d_model, patch_size, patch_size), # patch
+            )
+        self.pos_enc = RoPE(d_model, seq_len=200, base=10000)
+        # self.pos_emb = nn.Parameter(torch.randn(1, 8*8, d_model))
+        # self.pos_emb = nn.Parameter(RoPE2D(dim=d_model, h=8, w=8, base=10000).unsqueeze(0), requires_grad=False)
+
+        self.transformer = nn.Sequential(*[AttentionBlock(d_model, d_head=d_head) for _ in range(nlayers)])
+
+        # out_dim = out_dim or d_model
+        self.lin = nn.Linear(d_model, out_dim)
+        self.attn_pool = nn.Linear(d_model, 1, bias=False)
+
+        dim_v = d_model * 4
+        self.exp = nn.Sequential(
+            nn.Linear(out_dim, dim_v), act,
+            nn.Linear(dim_v, dim_v), act,
+            nn.Linear(dim_v, dim_v, bias=False),
+            )
+
+    def forward(self, x): # [b,t,d] / [b,c,h,w]
+        x = self.embed(x).flatten(2).transpose(1,2) # [b,c,h,w]->[b,h*w,c]
+        # x = self.embed(x.transpose(-2,-1)).transpose(-2,-1) # [b,t,d]
+        x = self.pos_enc(x)
+        # x = x + self.pos_emb
+
+        x = self.transformer(x)
+
+        attn = self.attn_pool(x).squeeze(-1) # [batch, seq] # seq_pool
+        out = (torch.softmax(attn, dim=-1).unsqueeze(1) @ x).squeeze(1) # [batch, 1, seq] @ [batch, seq, dim] -> [batch, dim]
+        out = self.lin(out)
+        return out # [seq_len, batch, ntoken]
+
+    def expand(self, x):
+        sx = self.forward(x)
+        vx = self.exp(sx)
+        return vx
+
+batch, seq_len, d_model = 4,3500,512
+in_dim, out_dim=3,16
+model = TransformerVICReg(in_dim, d_model, out_dim, d_head=4, nlayers=2, drop=0.).to(device)
+# x =  torch.rand((batch, seq_len, in_dim), device=device)
+x =  torch.rand((batch, in_dim, 32,32), device=device)
+out = model(x)
+print(out.shape)
+
+
+# @title Violet
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def off_diagonal(x):
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+class Violet(nn.Module):
+    def __init__(self, in_dim=3, d_model=32, out_dim=None, nlayers=2, d_head=4):
+        super().__init__()
+        out_dim = out_dim or d_model
+        self.student = TransformerVICReg(in_dim, d_model, out_dim=out_dim, d_head=d_head, nlayers=nlayers, drop=0.)
+        import copy
+        self.teacher = copy.deepcopy(self.student)
+        self.teacher.requires_grad_(False)
+
+        # vicreg
+        self.sim_coeff=25. # 10.0 # 25.0 # λ
+        self.std_coeff=25. # 1.0 # 25.0 # µ
+        self.cov_coeff=1. # 25.0 # 1.0 # ν
+
+        self.classifier = nn.Linear(out_dim, 18) # 10 18
+
+    def loss(self, x): # [batch, T, 3]c/ [b,c,h,w]
+        # print(x.shape)
+        vx = self.student.expand(x) # [batch, num_context_toks, out_dim]
+        with torch.no_grad(): vy = self.teacher.expand(x.detach()) # [batch, num_trg_toks, out_dim]
+        loss = self.vicreg(vx, vy)
+        return loss
+
+    def forward(self, x): # [batch, T, 3]
+        return self.student(x)
+
+    def classify(self, x): # [batch, T, 3]
+        sx = self.forward(x)
+        out = self.classifier(sx)
+        return out
+
+
+    # https://arxiv.org/pdf/2105.04906.pdf
+    def vicreg(self, x, y): # https://github.com/facebookresearch/vicreg/blob/main/main_vicreg.py
+        # invariance loss
+        repr_loss = F.mse_loss(x, y) # s(Z, Z')
+
+        x = x - x.mean(dim=0)
+        y = y - y.mean(dim=0)
+
+        # variance loss
+        std_x = torch.sqrt(x.var(dim=0) + 0.0001) #ϵ=0.0001
+        std_y = torch.sqrt(y.var(dim=0) + 0.0001)
+        std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2
+
+        batch_size, num_features = x.shape
+
+        if x.dim() == 1: x = x.unsqueeze(0)
+        if y.dim() == 1: y = y.unsqueeze(0)
+
+        # # covariance loss
+        cov_x = (x.T @ x) / (batch_size - 1) #C(Z)
+        cov_y = (y.T @ y) / (batch_size - 1)
+        cov_loss = off_diagonal(cov_x).pow_(2).sum().div(num_features)\
+         + off_diagonal(cov_y).pow_(2).sum().div(num_features) #c(Z)
+        loss = self.sim_coeff * repr_loss + self.std_coeff * std_loss + self.cov_coeff * cov_loss
+        print("in vicreg ",self.sim_coeff * repr_loss.item() , self.std_coeff * std_loss.item() , self.cov_coeff * cov_loss.item())
+        # return loss
+        return repr_loss, std_loss, cov_loss
+
+
+violet = Violet(in_dim=3, d_model=32, out_dim=16, nlayers=2, d_head=4).to(device)
+voptim = torch.optim.AdamW(violet.parameters(), lr=1e-3) # 1e-3?
+# voptim = torch.optim.AdamW([{'params': violet.student.transformer.parameters()},
+#     {'params': violet.student.exp.parameters(), 'lr': 3e-3}], lr=1e-3, weight_decay=1e-2) # default 1e-2
+print(sum(p.numel() for p in violet.parameters() if p.requires_grad)) # 27584
+
+# x = torch.rand((2,1000,3), device=device)
+x = torch.rand((2,3,32,32), device=device)
+# x = torch.rand((2,1,16), device=device)
+loss = violet.loss(x)
+# print(out.shape)
+print(loss)
+
+class Classifier(nn.Module):
+    def __init__(self, in_dim, num_classes=10):
+        super().__init__()
+        self.classifier = nn.Linear(in_dim, num_classes)
+    def forward(self, x): return self.classifier(x)
+classifier = Classifier(16).to(device)
+# classifier = Classifier(16, 18).to(device)
+coptim = torch.optim.SGD(classifier.parameters(), lr=1e-3)
+
+
+
